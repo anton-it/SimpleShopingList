4b7ea5759e7f51b726780b8e00005fb2773ea034
Admin1 <a89086829981@gmail.com>
2022-08-04 10:56:07 +0300

    lesson1

--- app/src/main/java/com/ak87/simpleshopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/ak87/simpleshopinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ak87.simpleshopinglist
+package com.ak87.simpleshopinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ak87.simpleshopinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
