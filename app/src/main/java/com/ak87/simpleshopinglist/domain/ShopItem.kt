2e7b952587caa444c8095e42a2945076f5863c1d
Admin1 <a89086829981@gmail.com>
2022-08-08 09:51:11 +0300

    lesson3 add data layer

--- app/src/main/java/com/ak87/simpleshopinglist/domain/ShopItem.kt
+++ app/src/main/java/com/ak87/simpleshopinglist/domain/ShopItem.kt
@@ -1,8 +1,16 @@
 package com.ak87.simpleshopinglist.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    //вынесем -1  константу т.к нельзя оставлять жестко закодированные числа
+//-1 означает что iD еще не установлен и ему нужно присвоить значение при добавлении в коллекцию.
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
+
+
